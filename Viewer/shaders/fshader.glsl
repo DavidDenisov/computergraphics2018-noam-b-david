eadfa948a6514f1fde38a85a23becf8933afe20f
Noam Blagovsky <noam7700@gmail.com>
2018-10-06 11:38:31 +0300

    some cleaning of the shaders code. just to delete expiriments
    nothing special

--- Viewer/shaders/fshader.glsl
+++ Viewer/shaders/fshader.glsl
@@ -1,14 +1,10 @@
 #version 330 core
 
 out vec4 FragColor;
-in vec3 vertexColor;
-in vec3 vertexPos;
+
 
 
 void main() 
 { 
 	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); //simple testing
-	FragColor = vec4(vertexColor, 1.0f); // in/out testing (from the vshader)
-
-	FragColor = vec4(vertexPos, 1.0f); //exrecise
 }
--- Viewer/shaders/vshader.glsl
+++ Viewer/shaders/vshader.glsl
@@ -1,12 +1,7 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
-out vec3 vertexColor;
-out vec3 vertexPos;
+
 void main()
 {
-     gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-	 vertexColor = aColor;
-
-	 vertexPos = aPos; //exrecise of shaders
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
 }
