814b8b06f829a2d196e41a5ba6714795828a7998
DavidDenisov <daviddenisov14@gmail.com>
2018-10-09 20:12:13 +0300

    Revert "Revert "Merge branch 'assigment3' of https://github.com/HaifaGraphics/computergraphics2018-noam-b-david into assigment3""
    This reverts commit bcac96e5932dc03df84dff017544ba696cd4b13b.

--- Viewer/shaders/fshader.glsl
+++ Viewer/shaders/fshader.glsl
@@ -76,6 +76,11 @@ void main()
 { 
 	//FragColor = norm; normal as color looks super cool 
 	//simple testing
+
+	vec4 transNorm = vec4(n, 0.0);
+	transNorm = NormMatrix * transNorm; //transform the normal! >:(
+	norm = vec3(transNorm.x, transNorm.y, transNorm.z);
+
 	int i=0;
 	if(! norm_as_color)
 	{
